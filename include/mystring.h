8bd77f3465614b4c8134f0b9457720e61eefb0f7
BSDSF23M035 <shahidmolecules@gmail.com>
2025-09-23 12:47:18 -0400

    work before static library: multifile build complete

---
+++ include/mystring.h
@@ -0,0 +1,12 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+#include <stddef.h>
+
+// Custom string length
+size_t mystrlen(const char *str);
+
+// Custom string copy
+void mystrcpy(char *dest, const char *src);
+
+#endif
